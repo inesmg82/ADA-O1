7ff791472425e8c0647e919ca8fdc73f6958d975
Ines M <imartinezgavin@gmail.com>
2022-10-05 11:42:21 +0200

    projecte final

--- 4 setmana/fept03-dom-inesmg82-main/FE-CAT-plain-JS-DOM-manipulation/index.js
+++ 4 setmana/fept03-dom-inesmg82-main/FE-CAT-plain-JS-DOM-manipulation/index.js
@@ -6,14 +6,16 @@ function click1() {
     let valorUsuari2 = document.getElementById(
         "projecte").value;
 
-
+    let valorUsuari3 = document.getElementById(
+        "description").value;
 
     document.getElementById("imprimir")
         .innerHTML += `
        <div id= "card">
         <img alt="imatge mal carregada" src="${valorUsuari}"
-        width=500" height="500"> 
-        <p>${valorUsuari2}</p>
+        width="500" height="500"> 
+        <p>${valorUsuari2}</p> 
+        <p>${valorUsuari3}</p>
         </div>
         `;
 }
